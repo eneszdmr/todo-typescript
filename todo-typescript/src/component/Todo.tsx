a0c7e0cf6db4d479cc16ea3ea902e7d535384266
Enes ÖZDEMİR <enes.ozdemir@botas.dom>
2024-08-02 17:03:51 +0300

    redux update hooks

--- todo-typescript/src/component/Todo.tsx
+++ todo-typescript/src/component/Todo.tsx
@@ -4,7 +4,9 @@ import { FaCheckDouble } from "react-icons/fa";
 import { TiEdit } from "react-icons/ti";
 import { TodoType } from "../types/Types";
 import { useDispatch } from "react-redux";
-import { removeTodo } from "../redux/todoSlice";
+import { removeTodo, updateTodo } from "../redux/todoSlice";
+import { useState } from "react";
+import { format } from "date-fns";
 
 
 interface Todoprops{
@@ -17,18 +19,41 @@ interface Todoprops{
 function Todo({ itemprops }: Todoprops) {
   const { id, content, date } = itemprops;
   const dispatch = useDispatch();
+  const updateDate = new Date()
+  const formattedDate = format(updateDate, 'yyyy-MM-dd HH:mm:ss');
+  const [editable, setEditable] = useState<Boolean>(false);
+
+  const [newTodo, setNewTodo] = useState<string>(content);
+
   function removeFromList() {
-    console.log(id);
+ 
     dispatch(removeTodo(id))
   }
+ 
+  function handleUpdateTodo() {
+    const payload = {
+      id,
+      content: newTodo,
+      date :formattedDate
+    }
+    dispatch(updateTodo(payload))
+    setEditable(false)
+  }
+
   return (
     <div className='single-todo'>
-      <div key={id}>{content} </div>
+      {editable ? <input style={{width:'250px', border:'none', borderBottom:'2px solid red', outline:'none'}}
+        type="text" value={newTodo}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setNewTodo(e.target.value)} /> :
+        <div key={id}>{content} </div>}
+     
       
       <div className='icons'>
-        <div style={{fontSize:'12px', marginRight:'15px'}}>{date.toString()} </div>
-        <RiDeleteBin6Line onClick={removeFromList} style={{marginRight:'9px'}}/>
-        <TiEdit/>
+        <div style={{fontSize:'12px', marginRight:'15px', opacity:'0.5'}}>{date.toString()} </div>
+        <RiDeleteBin6Line onClick={removeFromList} style={{ marginRight: '9px' }} />
+        
+        {editable ? <FaCheckDouble onClick={handleUpdateTodo} /> :  <TiEdit onClick={()=> setEditable(true)} />}
+        
       </div>
     </div>
   )
--- todo-typescript/src/redux/todoSlice.tsx
+++ todo-typescript/src/redux/todoSlice.tsx
@@ -18,11 +18,15 @@ export const todoSlice = createSlice({
         removeTodo: (state: TodoInitialState, action: PayloadAction<number>) => {
             state.todos = [...state.todos.filter((todo: TodoType) => todo.id !== action.payload)];
             
+        },
+        updateTodo: (state: TodoInitialState, action: PayloadAction<TodoType>) => {
+            state.todos=[...state.todos.map((todo:TodoType)=> todo.id !==action.payload.id ? todo : action.payload)]
+            
         }
 
     }
 })
 
 
-export const {createTodo,removeTodo}=todoSlice.actions
+export const {createTodo,removeTodo,updateTodo}=todoSlice.actions
 export default todoSlice.reducer
